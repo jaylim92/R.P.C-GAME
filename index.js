a333f102175d927b78d94aff8299cb4eab07e609
jaylim92 <jaylim3272@gmail.com>
2022-06-22 22:14:46 +0900

    가위바위보 완성

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const sBtn = document.querySelector(".sissor");
+const computerSelect = document.querySelector(".computer-select");
+const startGameBtn = document.querySelector(".start-game-button");
+const sBtn = document.querySelector(".scissor");
 const rBtn = document.querySelector(".rock");
 const pBtn = document.querySelector(".paper");
 
 const imogi = ["✌️", "✊", "🖐"];
-let index = 0;
 
-function aiSelect() {
-  const computerSelect = document.querySelector(".computer-select");
-  setInterval(() => {
+let index = 0;
+let changeSelect = undefined;
+
+function randomSelect() {
+  changeSelect = setInterval(() => {
     imogi[index++];
     if (index == imogi.length) {
       index = 0;
@@ -16,4 +19,29 @@ function aiSelect() {
   }, 50);
 }
 
-document.addEventListener("DOMContentLoaded", aiSelect);
+function gameResult(event) {
+  let final = event.target.outerText;
+  console.log(final);
+  if (final !== imogi[index]) {
+    return alert("you Win");
+  } else if (final == imogi[index]) {
+    return alert("draw");
+  } else {
+    return alert("You Loose");
+  }
+}
+
+function gameStart() {
+  startGameBtn.style.visibility = "hidden";
+  randomSelect();
+}
+function stopGame(event) {
+  startGameBtn.style.visibility = "";
+  clearInterval(changeSelect);
+  gameResult(event);
+}
+
+startGameBtn.addEventListener("click", gameStart);
+sBtn.addEventListener("click", stopGame);
+rBtn.addEventListener("click", stopGame);
+pBtn.addEventListener("click", stopGame);
